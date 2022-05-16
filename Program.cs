fd0d78c3ec2d8b59ea773ba9dee9cbdccee3280f
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:17:56 -0400

    Agregando Proyecto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n, pot, acum = 1;
+
+
+Console.WriteLine("Introdzca el Numero a elevar: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Introduzca la potencia: ");
+pot = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=pot; i++){
+    acum = acum * n;
+}
+
+Console.WriteLine("El resultado es {0}: ", acum);
+
+return acum;
